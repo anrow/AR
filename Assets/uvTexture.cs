c6888410690f7dd41fdab018c33456ea24170771
anrow <602492242anrow@gmail.com>
2017-09-28 18:02:52 +0900

    Enemy face .

---
+++ Assets/uvTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uvTexture : MonoBehaviour {
+
+    public Vector2 face;
+
+    public Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+
+        face = new Vector2(0f, -0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.material.SetTextureOffset("_MainTex", face);
+	}
+}
